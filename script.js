7977729e42a6364d856731d07914603332cacbd2
golam71 <74001687+golam71@users.noreply.github.com>
2025-06-05 18:22:24 +0600

    :art: Update form labels and styles for improved clarity and error handling

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
-const name
-const 
+const username = document.getElementById("name");
+const email = document.getElementById("email");
+const comment = document.getElementById("comment");
+const message = document.getElementById("message");
+
+function setErrorMessage(msg) {
+  message.classList.add("error");
+  message.innerText = msg;
+}
+
+if (username.value.length > 10) {
+}
